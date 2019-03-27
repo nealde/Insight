fbb7d4a275c5edc7a167deb21a8162488257c55c
neal <nealde@uw.edu>
2019-03-27 07:39:33 -0700

    profiled cython code, there was still python in the tight for loop due to using the loop-generated value in some math.  By initializing a placeholder value, the expected speedup is achieved, ~200x compared to python.

--- prototype/mfun.pyx
+++ prototype/mfun.pyx
@@ -5,12 +5,14 @@ cimport cython
 @cython.boundscheck(False)
 @cython.wraparound(False)
 def Msum_c(int n):
-    cdef double megaN = 1000000*n
-    cdef double s = 0.0
-    cdef double d0 = 0.0
-    cdef double d1 = 0.0
-    for k in range(1000000):
-        d0 = 4*(k+1+megaN)+1
-        d1 = d0-2
-        s += ((1.0/d0)-(1.0/d1))
-    return s
+	cdef int megaN = 1000000*n
+	cdef float s = 0.0
+	cdef int d0 = 0
+	cdef int d1 = 0
+	cdef int i = 0
+	for k in range(1000000):
+		i = k
+		d0 = 4*(i+1+megaN)+1
+		d1 = d0-2
+		s += ((1.0/d0)-(1.0/d1))
+	return s
