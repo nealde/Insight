d2058043601fe1e4e143057b9af5654eeebac13c
Ubuntu <ubuntu@ip-10-0-0-10.us-west-2.compute.internal>
2019-04-16 17:55:40 +0000

    updated some of the app interfaces

--- app/kafka_listen.py
+++ app/kafka_listen.py
@@ -1,4 +1,5 @@
 import kafka
+import time
 # connect to Kafka cluster
 cluster = kafka.KafkaClient("10.0.0.11:9092")
 topic="cloud"
@@ -6,11 +7,18 @@ consumer_group = 'default_group'
 
 
 # consume all messages
-global kafka_count
-kafka_count = 0
+#kafka_count = 0
+#global kafka_count
 
-def listen():
-    consumer = kafka.SimpleConsumer(cluster, consumer_group, topic)
+def listen(kafka_count):
+    consumer = kafka.SimpleConsumer(cluster,group=None, topic=topic, partitions=[0, 1])
+    print(consumer)
     for raw in consumer:
+        print(len(raw))
         kafka_count+=1
     return kafka_count
+n = 0
+while True:
+    n = listen(n)
+    time.sleep(1)
+    print(n)
